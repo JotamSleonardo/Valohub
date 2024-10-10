f4ed9598b8505092cb27ee679dea8010bdd47ce1
jotam Leonardo <jotam.leonardo@ffuf.de>
2024-10-10 17:26:08 +0800

    Initial Commit

---
+++ ValoHub/ValoHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValoHubApp.swift
+//  ValoHub
+//
+//  Created by Jotam Leonardo on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ValoHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ValoHubTests/ValoHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  ValoHubTests.swift
+//  ValoHubTests
+//
+//  Created by Jotam Leonardo on 10/10/24.
+//
+
+import Testing
+@testable import ValoHub
+
+struct ValoHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
